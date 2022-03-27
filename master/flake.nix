ad7b67d19998d89a5fee36b97edc1f7c0d70197d
hugosenari <hugosenari@gmail.com>
2022-03-27 17:59:10 -0300

    chore: re index chonker

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-chonker-master.ref   = "refs/heads/master";
   inputs.src-chonker-master.owner = "juancarlospaco";
   inputs.src-chonker-master.repo  = "nim-chonker";
+  inputs.src-chonker-master.dir   = "";
   inputs.src-chonker-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
