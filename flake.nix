c4d6e2e54d1e15145299b4de2a7a2e0af84becf2
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:58 -0300

    chore: re index chonker

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."chonker-master".dir   = "master";
-  inputs."chonker-master".owner = "nim-nix-pkgs";
-  inputs."chonker-master".ref   = "master";
-  inputs."chonker-master".repo  = "chonker";
-  inputs."chonker-master".type  = "github";
-  inputs."chonker-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."chonker-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."chonker-0_0_1".dir   = "0_0_1";
-  inputs."chonker-0_0_1".owner = "nim-nix-pkgs";
-  inputs."chonker-0_0_1".ref   = "master";
-  inputs."chonker-0_0_1".repo  = "chonker";
-  inputs."chonker-0_0_1".type  = "github";
-  inputs."chonker-0_0_1".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."chonker-0_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
